82d7735e5829f89da498b8450098dc8005866de5
mahdi <hadjidjmahdi@gmail.com>
2025-06-05 01:26:19 +0100

    quick accessibility fix

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NavBar() {
     return (
         <header className='w-full h-20 bg-white/1 backdrop-blur-[5px] px-16 z-50 fixed flex items-center justify-between'>
-            <div role='heading'>
+            <div>
                 <a aria-label='home section Link' href="#home"><h1 className='text-4xl text-white font-black'>Mahdi</h1></a>
             </div>
             <div role='navigation' className='flex items-center capitalize font-bold text-lg'>
