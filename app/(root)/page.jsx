053291a2e73edc89fa595b5379763e4a0bfc1904
mahdi <hadjidjmahdi@gmail.com>
2025-06-06 00:04:10 +0100

    performance optimize

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -17,11 +17,11 @@ export default function main() {
       <Image alt='left light' src="/Ellipse.svg" width={1000} height={1000} className="fixed select-none -top-56 -left-125 pointer-events-none" />
       <Image alt='right light' src="/Ellipse.svg" width={1000} height={1000} className="fixed select-none -bottom-110 -right-110 pointer-events-none" />
       <div role='navigation' className='social-icons fixed bottom-8 right-8 z-50 flex flex-col items-center justify-between gap-6 '>
-        <img src="/line 1.svg" alt='line' />
+        <Image src="/line 1.svg" width={1.5} height={60} alt='line' />
         <a target='_blank' aria-label='github Link' href="https://github.com/mahdii-Dz"><i className="fi fi-brands-github text-2xl flex text-customGray-300 hover:text-customGray-200"></i></a>
         <a target='_blank' aria-label='linkedin Link' href="https://www.linkedin.com/in/mahdihadjidj/"><i className="fi fi-brands-linkedin text-[26px] flex justify-center text-customGray-300 hover:text-customGray-200"></i></a>
         <a target='_blank' aria-label='instagram Link' href="https://www.instagram.com/mahdi.hadjidj/"><i className="fi fi-brands-instagram text-2xl flex text-customGray-300 hover:text-customGray-200"></i></a>
-        <img src="/line 1.svg" alt='line' />
+        <Image src="/line 1.svg" width={1.5} height={60} alt='line' />
       </div>
       <NavBar />
       <Home className='z-50' />
