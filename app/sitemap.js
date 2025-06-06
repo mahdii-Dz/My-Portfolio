3525844f0631ba59f6548b09c0f74b4ab0279eff
mahdi <hadjidjmahdi@gmail.com>
2025-06-06 14:17:21 +0100

    adding sitemap and robots

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+
+
+export default function sitemap() {
+    return [
+        {
+            url: "hhttps://mahdiiportfolio.netlify.app/",
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+    ]
+}
