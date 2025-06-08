e53d90af2d7155f2a8dc995cf90ce9ce0699e9a2
mahdi <hadjidjmahdi@gmail.com>
2025-06-08 16:34:15 +0100

    nav fix

--- app/components/MobileNav.jsx
+++ app/components/MobileNav.jsx
@@ -18,11 +18,11 @@ function MobileNav() {
             </div>
             <aside
                 ref={ref}
-                className={`flex absolute overflow-auto top-0 right-0 w-72 h-dvh bg-white/30 backdrop-blur-[20px]  flex-col items-center font-bold justify-between pt-12 pb-6 px-4 ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition duration-300 ease-in-out`}>
+                className={`flex absolute overflow-auto top-0 right-0 w-72 h-dvh bg-white/30 backdrop-blur-[20px] flex-col items-center font-bold justify-between pt-12 pb-6 px-4 ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition duration-300 ease-in-out`}>
                 <div onClick={() => setIsOpen(!isOpen)} className='exitBtn absolute top-2 left-2 text-3xl text-customGray-200'>
                     <Image aria-label='exit button' src="/X.svg" width={32} height={32} alt="X" />
                 </div>
-                <nav role='navigation' aria-label="Main navigation" className='text-white mt-4 text-2xl flex flex-col items-center gap-12 w-full'>
+                <nav role='navigation' aria-label="Main navigation" className='text-white mt-4 z-50 text-2xl flex flex-col items-center gap-12 w-full'>
                     <a className='w-full h-16 bg-customGray-300 rounded-lg text-center pt-4' aria-label='about section Link' href="#about">about</a>
                     <a className='w-full h-16 bg-customGray-300 rounded-lg text-center pt-4' aria-label='skills section Link' href="#skills">skills</a>
                     <a className='w-full h-16 bg-customGray-300 rounded-lg text-center pt-4' aria-label='projects section Link' href="#projects">projects</a>
